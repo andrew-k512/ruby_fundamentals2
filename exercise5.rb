16193915d12610f2fe07c115ae90fdca2f8995af
andrew-k512 <andrew.fiveonetwo@gmail.com>
2017-06-27 15:43:00 -0400

    finish exercise 6

--- exercise5.rb
+++ exercise5.rb
@@ -1,10 +1,7 @@
-# Lines 2 & 3 ask user for F degress in a string value.
-puts "What is the temp in fahrenheit?"
-f_temp = gets.chomp
-# Line 4 defines the equation to calculate C degrees.
-c_temp = (f_temp.to_i - 32) * 5 / 9
-# Output value produce by forumula as a string to the user.
-puts "That is equal to #{c_temp} degrees celsius."
+def greet_backwards(name)
+puts "Hello #{name.reverse * 2}! Welcome home."
+end
 
-####### next line is a different way to do line seven. It says ___ F is ____ C.
-######|     puts f_temp.to_s + 'F is ' + c_temp.to_s + 'C.'
+puts "What is your name?"
+name = gets.chomp
+puts greet_backwards(name)
